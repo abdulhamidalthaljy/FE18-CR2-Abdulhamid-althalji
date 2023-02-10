a135bd661acac25df4d9cf3cfc5223fa51072a14
abdalhamedthaljy <59847991+abdalhamedthaljy@users.noreply.github.com>
2023-02-10 23:00:56 +0100

    ready

---
+++ js/json.js
@@ -0,0 +1,40 @@
+
+let todo = `[
+    {
+      "task": " Visit The Worlds Top Museums At MuseumsQuartier",
+      "desc": " Viennas MuseumsQuartier houses most of the must-see museums  in the old Hapsburgian Imperial stables of all places.",
+      "img": "https://inspiredbymaps.com/wp-content/uploads/2016/07/1375713_10151765319502762_1360726000_n-768x512.jpg.webp",
+      "priority": 0
+    },
+    {
+      "task": "Shop Up A Storm On Mariahilferstraße",
+      "desc": "Mariahilferstraße is where the Viennese head for all their shopping needs. Going for what seems like miles and miles,",
+      "img": "https://img.itinari.com/pages/images/original/d433632b-b670-486a-88dc-201f475d1d5b-radub85.jpg?ch=DPR&dpr=2.625&w=1200&h=800&s=70159b46491e012407d3f2ab467b7467",
+      "priority": 0
+    },
+    {
+      "task": " Game Of Ice Hockey In Vienna",
+      "desc": "The Austrian Hockey league (also including teams from neighboring countries) starts from mid-September and really is one of the top things to do in Vienna.",
+      "img": "https://newsroom.ice.hockey/Content/665057/66b3f205-77a8-477e-bc8f-27b593c9d6b0/1200/2400/.jpg",
+      "priority": 0
+    },
+    {
+      "task": "Try The Famous Vienna Schnitzel",
+      "desc": "Wiener schnitzel, possibly one of the best-known specialties of Viennese cuisine, is a national dish of Austria ",
+      "img": "https://cdn.pixabay.com/photo/2018/10/28/19/44/schnitzel-3779726_960_720.jpg",
+      "priority": 0
+    },
+    {
+      "task": "Walk Between The Mountains And Lakes In Ancient Hallstatt",
+      "desc": "Hallstatt is a small mountain village in Western Austria, roughly an hour from the second city of Salzburg as you head through the mountains",
+      "img": "https://cdn.pixabay.com/photo/2018/08/16/08/39/hallstatt-3609863_960_720.jpg",
+      "priority": 0
+    },
+    {
+      "task": "Wander Along The Magical Danube Island",
+      "desc": "Spend a day in the sun at the Danube Island Easily accessible by U-Bahn or Bike the Danube Island is a 21km island in the Danube River",
+      "img": "https://e8s3v3w7.rocketcdn.me/wp-content/uploads/2017/11/occ88sterreich-wien-die-donauinsel-in-wien-trennt-seit-1988-die-alte-von-der-neuen-donau-und-ist-als-naherholungsgebiet-und-veranstaltungsort-bekannt-occ88sterreich-mrgb-shutterstock-696x464.jpg",
+      "priority": 0
+    }
+    
+  ]`
---
+++ js/main.js
@@ -0,0 +1,82 @@
+
+let dolist = JSON.parse(todo)
+console.log(dolist)
+for (show of dolist) {
+    document.getElementById("imgContainer").innerHTML +=
+
+        `</div>
+    <div class="card shadow-lg  mb-5 bg-body-tertiary rounded  "">
+    <div class="d-flex justify-content-between">
+    <p class=" btn btn-info  " > Task</p>
+    <p><i class="ri-bookmark-line btn btn-light"></i> <i class="ri-more-2-line btn btn-light"></i></p>
+</div>
+  <img src="${show.img}" class="card-img-top foto" alt="${show.task}">
+  <div class="card-body">
+    <p class="card-text text-danger h4">${show.task} </p>
+    <p class="card-text h6">${show.desc}</p>
+    <hr>
+    <p class="card-text btn btn-light prioritybtn "> <i class="ri-error-warning-line"></i>  Priority level <span class="text-light btn btn-success prioritys" > ${show.priority} </span></p>
+    <p class="card-text ">  <label for="start">Dead line:</label>
+
+    <input type="date" id="start" name="trip-start"
+           value="2023-10-02"
+           min="2023-01-01" max="2023-12-31"></p>
+           <hr>
+           <div  class="d-flex justify-content-end gap-2">
+    <p class="btn btn-danger  delet "> <i class="ri-delete-bin-6-line"></i> Delete</p> 
+     <p class="btn  btn-success "><i class="ri-check-double-line"></i> Done </p></div>
+  </div>
+</div> </div>`
+}
+// priority counter
+let btns = document.getElementsByClassName("prioritybtn");
+
+for (let i = 0; i < btns.length; i++) {
+
+    btns[i].addEventListener("click", function () {
+        dolist[i].priority++;
+        document.getElementsByClassName("prioritys")[i].innerHTML = dolist[i].priority
+
+        let prio = dolist[i].priority
+        //    the background color of the priority level number will change depending on the displayed number
+        if (prio == 0 || prio == 1) {
+            document.getElementsByClassName("prioritys")[i].classList.add("bg-success");
+        } else if (prio == 2 || prio == 3) {
+            document.getElementsByClassName("prioritys")[i].classList.add("bg-warning");
+        } else if (prio == 4 || prio == 5) {
+            document.getElementsByClassName("prioritys")[i].classList.add("bg-danger");
+        }
+
+    })
+
+}
+console.log(dolist[0])
+
+// delet card
+let del = document.getElementsByClassName("delet");
+
+for (let i = 0; i < del.length; i++) {
+
+    del[i].addEventListener("click", function () {
+
+        document.getElementsByClassName("card")[i].style.display = "none";
+
+    })
+}
+
+//  Add a Sort button that will sort the resulting list of tasks according to their current level of importance 
+// const sort = document.getElementsByClassName("sort-btn");
+// sort.addEventListener("click", function () {
+
+//     document.getElementsByClassName("card").style.order = "4";
+// }, false);
+
+
+
+
+
+
+
+
+
+
